6106242c812823bb4b642e55f59c04aa2d393462
oliver <oliver@Big-Mac.local>
2009-10-20 19:47:33 -0200

    removing taxon description field from translations, it no longer exists on the updated taxon model

--- db/migrate/20090917023447_add_taxonomy_description_translation.rb
+++
@@ -1,9 +0,0 @@
-class AddTaxonomyDescriptionTranslation < ActiveRecord::Migration
-  def self.up
-    add_column :taxon_translations, :description, :text
-  end
-
-  def self.down
-    remove_column :taxon_translations, :description
-  end
-end
