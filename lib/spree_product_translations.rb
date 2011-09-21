7907b8764e9c849a72b22e06e7d89b5edb1ab5b1
Jeroen Jacobs <git@jeroenj.be>
2011-09-21 15:32:56 +0200

    Set fallbacks_for_empty_translations preference through Spree config

--- lib/spree_product_translations.rb
+++ lib/spree_product_translations.rb
@@ -10,32 +10,34 @@ module SpreeProductTranslations
         Rails.env.production? ? require(c) : load(c)
       end
 
+      @@fallbacks_for_empty_translations = Spree::Config[:fallbacks_for_empty_translations] != '0' # Booelans are stored as an int as a string
+
       ::Product.class_eval do
-        translates :name, :description, :meta_description, :meta_keywords, :fallbacks_for_empty_translations => true
+        translates :name, :description, :meta_description, :meta_keywords, :fallbacks_for_empty_translations => @@fallbacks_for_empty_translations
       end
 
       ::Property.class_eval do
-        translates :presentation, :fallbacks_for_empty_translations => true
+        translates :presentation, :fallbacks_for_empty_translations => @@fallbacks_for_empty_translations
       end
 
       ::Prototype.class_eval do
-        translates :name, :fallbacks_for_empty_translations => true
+        translates :name, :fallbacks_for_empty_translations => @@fallbacks_for_empty_translations
       end
 
       ::Taxonomy.class_eval do
-        translates :name, :fallbacks_for_empty_translations => true
+        translates :name, :fallbacks_for_empty_translations => @@fallbacks_for_empty_translations
       end
 
       ::Taxon.class_eval do
-        translates :name, :description, :fallbacks_for_empty_translations => true
+        translates :name, :description, :fallbacks_for_empty_translations => @@fallbacks_for_empty_translations
       end
 
       ::OptionType.class_eval do
-        translates :presentation, :fallbacks_for_empty_translations => true
+        translates :presentation, :fallbacks_for_empty_translations => @@fallbacks_for_empty_translations
       end
 
       ::OptionValue.class_eval do
-        translates :presentation, :fallbacks_for_empty_translations => true
+        translates :presentation, :fallbacks_for_empty_translations => @@fallbacks_for_empty_translations
       end
 
       # Enable I18n fallbacks
